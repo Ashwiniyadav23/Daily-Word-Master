30a3151aceb84e55656ff210ce760570bca06665
Ashwini Yadav <ashwiniyadav23@navgurukul.org>
2025-06-09 12:35:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     setLoading(true);
     setError("");
     setQuizStarted(false); // Reset quiz on level change
-    fetch(`https://word-backend-black.vercel.app`)
+    fetch(`https://word-backend-black.vercel.app/api/word?level=${level}`)
       .then((res) => {
         if (!res.ok) throw new Error("Failed to fetch data");
         return res.json();
